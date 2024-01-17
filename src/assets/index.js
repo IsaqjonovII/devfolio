5c07f5cf76278d4388177a878b71f195e3a6126e
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2024-01-17 17:27:58 +0500

    home page is ready

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,5 @@
 import myLogo from "./my-logo.webp";
 import myResume from "./Resume.pdf";
-import coder from "./coder.png";
-import logoGif from "./III.gif";
-import me from "./me.webp";
-import meMobile from "./me_mobile.webp";
+import coder from "./me.png";
 
-export { myLogo, myResume, coder, logoGif, me, meMobile };
+export { myLogo, myResume, coder };

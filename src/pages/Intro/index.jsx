5eac39a7cbe39189672e9d70d8edb2a74c95a6d2
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-01-22 20:49:02 +0500

    animate on scroll

--- src/pages/Intro/index.jsx
+++ src/pages/Intro/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Bounce } from "react-reveal";
+import { Fade } from "react-reveal";
 import c from "./style.module.css";
 import { introData, socialLinks } from "utils";
 import { coder } from "assets";
@@ -9,33 +9,48 @@ const Intro = () => {
     <main className={c.intro}>
       <div className="container">
         <section>
-          <h1 className={c.intro__title}>I&apos;m Ilhomjon</h1>
+          <h1 className={c.intro__title}>
+            <Fade duration={800} cascade>
+              I&apos;m Ilhomjon
+            </Fade>
+          </h1>
         </section>
         <div className={c.section__wrp + " flex"}>
-          <section>
-            <Bounce>
+          <section className={c.me}>
+            <Fade delay={150}>
               <img className={c.intro__img} src={coder} alt="" />
-            </Bounce>
+            </Fade>
             <div className={c.social__links}>
-              {socialLinks.map(({ key, link, label }) => (
-                <Bounce key={key}>
-                  <Link to={link} rel="noopener noreferrer" target="_blank">
-                    <div className={c.social__link}> {label} </div>
-                  </Link>
-                </Bounce>
+              {socialLinks.map(({ key, link, label }, index) => (
+                <Link
+                  key={key}
+                  to={link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <p className={c.social__link}>
+                    {" "}
+                    <Fade delay={index * 120}>{label}</Fade>{" "}
+                  </p>
+                </Link>
               ))}
             </div>
           </section>
+
           <section className={c.text__wrp}>
             {introData.map(({ id, title, content }) => (
               <div key={id}>
-                <h2 className={c.section__title}>{title}</h2>
-                {content}
+                <Fade delay={id * 100}>
+                  <h2 className={c.section__title}>{title}</h2>
+                  {content}
+                </Fade>
               </div>
             ))}
-            <div className="navigate__link">
-              <Link to="/portfolio">Explore my portfolio</Link>
-            </div>
+            <Fade delay={250}>
+              <div className="navigate__link">
+                <Link to="/portfolio">Explore my portfolio</Link>
+              </div>
+            </Fade>
           </section>
         </div>
       </div>
